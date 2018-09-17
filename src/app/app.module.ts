19e5a4a9f1e36b221c0a9409a68456b046ec08fb
Ananth Ganesan <mail2ananthece@gmail.com>
2018-09-17 22:33:21 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,6 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 //import { ApiServiceService } from './shared/api-service.service';
-
-import { AccordionModule } from 'primeng/primeng';
-import { PanelModule } from 'primeng/primeng';
-import { ButtonModule } from 'primeng/primeng';
-import { RadioButtonModule } from 'primeng/primeng';
 import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
 
 @NgModule({
@@ -24,12 +19,7 @@ import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule,
-    AccordionModule,
-    PanelModule,
-    ButtonModule,
-    RadioButtonModule,
-    MatSliderModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
