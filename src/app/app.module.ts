f9ed356122356c61b540c23efaf2f42116d58c63
Ananth Ganesan <mail2ananthece@gmail.com>
2018-09-18 09:21:59 +0000

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { MatSliderModule } from '@angular/material'
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
